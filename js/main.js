aef210b9b3c1ef5494a3bcce494c6a19e197fc64
Suraj Marakani <marakanisuraj@Surajs-MacBook-Pro.local>
2019-06-19 14:04:14 -0400

    asd

---
+++ js/main.js
@@ -0,0 +1,33 @@
+// carousel for quotes
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1} 
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; 
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+  slides[slideIndex-1].style.display = "block"; 
+  dots[slideIndex-1].className += " active";
+}
+
+$(window).scroll(function() {
+    var theta = $(window).scrollTop() / 10 % Math.PI;
+    $('#logo').css({ transform: 'rotate(' + theta + 'rad)' });
+});
